4a281a19f62647c0bf20a223eb7605dc2a1250bd
DorianLarouziere <dorian.larouziere@student.junia.com>
2024-02-27 21:51:08 +0100

    fix

--- PoseModule.py
+++ PoseModule.py
@@ -3,37 +3,83 @@ import sys
 from ultralytics import YOLO
 import math
 
-class poseDetector():
+class poseDetector:
+    """
+    Class for detecting and analyzing poses using YOLOv8 and OpenCV.
+
+    Attributes:
+    - model: YOLO model for detecting poses.
+    - keyPoints: Dictionary to store keypoints detected by the model.
+    """
 
     def __init__(self):
+        """
+        Initializes the pose detector object.
+
+        Initializes the YOLO model for pose detection and initializes an empty dictionary
+        to store keypoints.
+        """
         self.model = YOLO("yolov8m-pose.pt")
         self.keyPoints = dict()
 
     def findPose(self, img):
+        """
+        Detects poses in an image using the YOLO model.
+
+        Args:
+        - img: Image to detect poses in.
+
+        Returns:
+        - img: Image with detected poses.
+        """
         self.results = self.model.predict(img, verbose=False)
         return img
     
-    def findKeyPoints(self, img, draw = True):
-        kptss= self.results[0].keypoints.data
+    def findKeyPoints(self, img, draw=True):
+        """
+        Finds keypoints in an image.
+
+        Args:
+        - img: Image to find keypoints in.
+        - draw: Boolean indicating whether to draw keypoints on the image (default is True).
+        """
+        # Extract keypoints from YOLO results and store in self.keyPoints dictionary
+        kptss = self.results[0].keypoints.data
         for kpts in kptss:
             i = 0
             for kpt in kpts:
-                self.keyPoints[i] = (int(kpt[0]),int(kpt[1]))
-                if(draw):
-                    cv2.circle(img, (int(kpt[0]),int(kpt[1])), radius=0, color=(0, 255, 255), thickness=2)
-                i+=1
+                self.keyPoints[i] = (int(kpt[0]), int(kpt[1]))
+                # Draw keypoints on the image if draw is True
+                if draw:
+                    cv2.circle(img, (int(kpt[0]), int(kpt[1])), radius=0, color=(0, 255, 255), thickness=2)
+                i += 1
         
     
-    def findAngle(self, img, p1, p2, p3, draw = True):
+    def findAngle(self, img, p1, p2, p3, draw=True):
+        """
+        Finds the angle between three keypoints.
+
+        Args:
+        - img: Image to draw lines and keypoints on.
+        - p1: Index of the first keypoint.
+        - p2: Index of the second keypoint.
+        - p3: Index of the third keypoint.
+        - draw: Boolean indicating whether to draw lines and keypoints on the image (default is True).
+
+        Returns:
+        - angle: Angle between the three keypoints.
+        """
+        # Get coordinates of the keypoints
         k1 = self.keyPoints[p1]
         k2 = self.keyPoints[p2]
         k3 = self.keyPoints[p3]
         x1, y1 = k1[0], k1[1]
         x2, y2 = k2[0], k2[1]
         x3, y3 = k3[0], k3[1]
-        angle = int(math.degrees(math.atan2(y3-y2, x3-x2) - 
-                             math.atan2(y1-y2, x1-x2)))
+        # Calculate the angle using arctan2 and convert to degrees
+        angle = int(math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2)))
         
+        # Normalize angle to be between 0 and 180 degrees
         if angle < 0:
             angle += 360
             if angle > 180:
@@ -41,49 +87,18 @@ class poseDetector():
         elif angle > 100:
             angle = 360 - angle
 
-        if(draw):
-            cv2.line(img, (x1, y1), (x2, y2), (255,255,255), 3)
-            cv2.line(img, (x3, y3), (x2, y2), (255,255,255), 3)
-
+        # Draw lines and keypoints on the image if draw is True
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
             
-            cv2.circle(img, (x1, y1), 5, (0,0,255), cv2.FILLED)
-            cv2.circle(img, (x1, y1), 15, (0,0,255), 2)
-            cv2.circle(img, (x2, y2), 5, (0,0,255), cv2.FILLED)
-            cv2.circle(img, (x2, y2), 15, (0,0,255), 2)
-            cv2.circle(img, (x3, y3), 5, (0,0,255), cv2.FILLED)
-            cv2.circle(img, (x3, y3), 15, (0,0,255), 2)
+            cv2.circle(img, (x1, y1), 5, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x2, y2), 5, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x3, y3), 5, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
             
-            cv2.putText(img, str(int(angle)), (x2-50, y2+50), 
-                        cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255), 2)
+            cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
 
         return angle
-    
-def main():
-    detector = poseDetector()
-    cap = cv2.VideoCapture(0)
-
-
-    if not cap.isOpened():
-        print("Error reading video file")
-        sys.exit()
-
-    while cap.isOpened():
-        success, frame = cap.read()
-        if success:
-
-            detector.findPose(frame)
-            detector.findKeyPoints(frame)
-            angle = detector.findAngle(frame, 0, 1, 3)
-            print(f"angle = {angle}")
-
-            cv2.imshow("YOLOv8 Detection", frame)
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
-        else:
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
--- PushCounting.py
+++ PushCounting.py
@@ -1,43 +1,50 @@
+# Import necessary libraries
 import cv2
 import PoseModule as pm
 import sys
 import numpy as np
 
+# Main function
 def main():
+    # Initialize pose detector
     detector = pm.poseDetector()
+    # Capture video from default camera
     cap = cv2.VideoCapture(0)
 
+    # Check if the video capture is successful
     if not cap.isOpened():
         print("Error reading video file")
         sys.exit()
 
+    # Initialize variables
     count = 0
     direction = "down"
     isFormCorrect = False
     feedback = "Fix Form"
 
-    width = cap.get(3)
-    height = cap.get(4)
-
+    # Main loop for processing video frames
     while cap.isOpened():
         success, frame = cap.read()
         if success:
-
+            # Find pose and keypoints in the frame
             detector.findPose(frame)
             detector.findKeyPoints(frame)
+
+            # Calculate angles at various joints, for the elbow, shoulder and hip
             elbow_angle = detector.findAngle(frame, 10, 8, 6)
             shoulder_angle = detector.findAngle(frame, 8, 6, 12)
             hip_angle = detector.findAngle(frame, 6, 12, 14)
 
+            # Interpolate the angle of the elbow to determine the percentage of success of a pushup
             percentage_of_success_pushup = np.interp(elbow_angle, (90, 160), (0, 100))
-            bar_progress = np.interp(elbow_angle, (90, 160), (380, 50))
 
-            if elbow_angle > 160 & shoulder_angle > 40 & hip_angle > 160:
+            # Check if the form of the pushup is correct
+            if elbow_angle > 160 and shoulder_angle > 40 and hip_angle > 160:
                 isFormCorrect = True
 
             if isFormCorrect:
                 if percentage_of_success_pushup == 0:
-                    if elbow_angle <= 90 & hip_angle > 160:
+                    if elbow_angle <= 90 and hip_angle > 160:
                         feedback = "Up"
                         if direction == "down":
                             count += 0.5
@@ -46,7 +53,7 @@ def main():
                         feedback = "Fix Form"
                 
                 if percentage_of_success_pushup == 100:
-                    if elbow_angle > 160 & shoulder_angle > 40 & hip_angle > 160:
+                    if elbow_angle > 160 and shoulder_angle > 40 and hip_angle > 160:
                         feedback = "Down"
                         if direction == "up":
                             count += 0.5
@@ -54,32 +61,30 @@ def main():
                     else:
                         feedback = "Fix Form"
 
-
             if isFormCorrect:
                 cv2.rectangle(frame, (580, 50), (600, 380), (0, 255, 0), 3)
-            cv2.rectangle(frame, (580, int(bar_progress)), (600, 380), (0, 255, 0), cv2.FILLED)
-            cv2.putText(frame, f'{int(percentage_of_success_pushup)}%', (565, 430), cv2.FONT_HERSHEY_PLAIN, 2,
-                        (255, 0, 0), 2)
 
-
-            #Pushup counter
-            cv2.rectangle(frame, (0, 380), (100, 480), (0, 255, 0), cv2.FILLED)
-            cv2.putText(frame, str(int(count)), (25, 455), cv2.FONT_HERSHEY_PLAIN, 5,
-                        (255, 0, 0), 5)
+            # Draw pushup counter
+            cv2.rectangle(frame, (0, 380), (100, 480), (255, 255, 255), cv2.FILLED)
+            cv2.putText(frame, str(int(count)), (25, 455), cv2.FONT_HERSHEY_PLAIN, 5, (26, 7, 109), 5)
             
-            #feedback 
+            # Draw feedback message
             cv2.rectangle(frame, (500, 0), (640, 40), (255, 255, 255), cv2.FILLED)
-            cv2.putText(frame, feedback, (500, 40 ), cv2.FONT_HERSHEY_PLAIN, 2,
-                        (0, 255, 0), 2)
+            cv2.putText(frame, feedback, (500, 40 ), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
 
+            # Display the frame
             cv2.imshow("Pushup counter", frame)
+            # Break the loop if 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
         else:
             break
 
+    # Release the camera and close OpenCV windows
     cap.release()
     cv2.destroyAllWindows()
 
+# Check if the script is being run directly
 if __name__ == "__main__":
+    # Call the main function
     main()
